f26caf5284f25fd7819d0f7540f327a0a6b0a66e
Pedro Cano <pedro@findex.la>
2025-06-18 09:07:49 -0600

    first commit

---
+++ cmus/lib.pl
@@ -0,0 +1,236 @@
+/home/cano/Music/Cinematic Ambience/Table For Two.mp3
+/home/cano/Music/Cinematic Ambience/Emmett’s Room.mp3
+/home/cano/Music/Concentracion Jazzera/Legend-(p).mp3
+/home/cano/Music/Enfoque Electronico/Alone in Kyoto-(p).mp3
+/home/cano/Music/Enfoque Electronico/AIR - Cosmic Trip (from 𝐿𝑒 𝑣𝑜𝑦𝑎𝑔𝑒 𝑑𝑎𝑛𝑠 𝑙𝑎 𝑙𝑢𝑛𝑒 - Official Audio)-(p).mp3
+/home/cano/Music/Cinematic Ambience/The Midnight Sky.mp3
+/home/cano/Music/Enfoque Electronico/Alex Somers – Sooner (Film Vignette)-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Alfa Mist - ＂First Light＂-(p).mp3
+/home/cano/Music/Enfoque Electronico/Xerrox Île-(p).mp3
+/home/cano/Music/Enfoque Electronico/Bell Valley - Animal Feelings-(p).mp3
+/home/cano/Music/Enfoque Electronico/#20-(p).mp3
+/home/cano/Music/Enfoque Electronico/Boozoo Bajou - Nacht Plätscher-(p).mp3
+/home/cano/Music/Enfoque Electronico/M_L_02-(p).mp3
+/home/cano/Music/Cinematic Ambience/A. Coltrane： Radhe-Shyam.mp3
+/home/cano/Music/Enfoque Electronico/False Scale-(p).mp3
+/home/cano/Music/Enfoque Electronico/444-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Avishai Cohen Trio - Below (from the album ＂Shifting Sands＂)-(p).mp3
+/home/cano/Music/Enfoque Electronico/Towers-(p).mp3
+/home/cano/Music/Enfoque Electronico/Be Svendsen - Andromeda-(p).mp3
+/home/cano/Music/Enfoque Electronico/BICEP ｜ OPAL (FOUR TET REMIX)-(p).mp3
+/home/cano/Music/Enfoque Electronico/BICEP ｜ LIDO (Official Video)-(p).mp3
+/home/cano/Music/Enfoque Electronico/Perturbator - ＂Dethroned Under A Funeral Haze＂ [＂Lustful Sacraments＂ - 2021]-(p).mp3
+/home/cano/Music/Enfoque Electronico/Boards of Canada - Dayvan Cowboy-(p).mp3
+/home/cano/Music/Enfoque Electronico/Turquoise Hexagon Sun-(p).mp3
+/home/cano/Music/Enfoque Electronico/Seven Forty Seven-(p).mp3
+/home/cano/Music/Enfoque Electronico/Bonobo - Linked (Official Audio)-(p).mp3
+/home/cano/Music/Enfoque Electronico/One Eye Round the Warm Corner-(p).mp3
+/home/cano/Music/Enfoque Electronico/Luke Abbott - Brazil-(p).mp3
+/home/cano/Music/Cinematic Ambience/1883 Theme by Brian Tyler.mp3
+/home/cano/Music/Cinematic Ambience/＂Redeeming Love＂ Composed & Conducted by Brian Tyler feat. Gil Shaham.mp3
+/home/cano/Music/Cinematic Ambience/Standing Prayer.mp3
+/home/cano/Music/Concentracion Jazzera/Ay Amor (Live)-(p).mp3
+/home/cano/Music/Enfoque Electronico/Stasis 113-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Tones for Joan's Bones-(p).mp3
+/home/cano/Music/Cinematic Ambience/It Can't Be!.mp3
+/home/cano/Music/Cinematic Ambience/End Game.mp3
+/home/cano/Music/Enfoque Electronico/Sea-(p).mp3
+/home/cano/Music/Enfoque Electronico/08 Cylob - I Left My Heart in Syntax Error [Cylob Industries]-(p).mp3
+/home/cano/Music/Enfoque Electronico/Daniel Avery - Projector (Official Video)-(p).mp3
+/home/cano/Music/Cinematic Ambience/I Care About You.mp3
+/home/cano/Music/Cinematic Ambience/Strange Statue.mp3
+/home/cano/Music/Enfoque Electronico/DJ Ocean - Ocean's Theme (Official Music Video)-(p).mp3
+/home/cano/Music/Cinematic Ambience/Broken Glass.mp3
+/home/cano/Music/Enfoque Electronico/Space Race-(p).mp3
+/home/cano/Music/Cinematic Ambience/Into the Light (Ins Licht).mp3
+/home/cano/Music/Concentracion Jazzera/Too Damn Hot (Live)-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Chimes-(p).mp3
+/home/cano/Music/Cinematic Ambience/Fata Morgana.mp3
+/home/cano/Music/Enfoque Electronico/Our Utopie (Extended Version)-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Poster-(p).mp3
+/home/cano/Music/Enfoque Electronico/Nils Frahm - About Coming and Leaving-(p).mp3
+/home/cano/Music/Enfoque Electronico/Nils Frahm - O I End-(p).mp3
+/home/cano/Music/Cinematic Ambience/Tüür： Phantasma.mp3
+/home/cano/Music/Concentracion Jazzera/Mindfulness (Live in studio)-(p).mp3
+/home/cano/Music/Enfoque Electronico/Faodail & Hildur Höglind - Changing-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Nago-(p).mp3
+/home/cano/Music/Cinematic Ambience/Aurora.mp3
+/home/cano/Music/Concentracion Jazzera/Movement 5-(p).mp3
+/home/cano/Music/Enfoque Electronico/Four Tet - Pinnacles-(p).mp3
+/home/cano/Music/Enfoque Electronico/Four Tet - Two Thousand and Seventeen-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Cabana-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Theme For Leon-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Caithness-(p).mp3
+/home/cano/Music/Cinematic Ambience/Gabríel Ólafs - The Monolith (Audio).mp3
+/home/cano/Music/Cinematic Ambience/Gabríel Ólafs - The True Meaning Of Forever + Coda (Audio).mp3
+/home/cano/Music/Cinematic Ambience/Gabríel Ólafs - Whale Overture.mp3
+/home/cano/Music/Enfoque Electronico/Helios - One And The Same-(p).mp3
+/home/cano/Music/Concentracion Jazzera/＂2021＂ (Gustav Lundgren, Karl-Henrik Ousbäck, Pär-Ola Landin)-(p).mp3
+/home/cano/Music/Enfoque Electronico/Hammock - We Watched You Disappear (Universalis)-(p).mp3
+/home/cano/Music/Cinematic Ambience/It Comes in Waves (Live at Studio 1, Warsaw).mp3
+/home/cano/Music/Cinematic Ambience/Herald of the Change.mp3
+/home/cano/Music/Cinematic Ambience/Stranded.mp3
+/home/cano/Music/Cinematic Ambience/Hans Zimmer - Harry Haft ｜ The Survivor (Original Motion Picture Soundtrack).mp3
+/home/cano/Music/Cinematic Ambience/The Rock Suite (Official Audio) ｜ The World of Hans Zimmer.mp3
+/home/cano/Music/Cinematic Ambience/Kingdoms of Ice.mp3
+/home/cano/Music/Cinematic Ambience/Warming Up My Instruments.mp3
+/home/cano/Music/Cinematic Ambience/Penny Returns (Interlude).mp3
+/home/cano/Music/Concentracion Jazzera/Beyond-(p).mp3
+/home/cano/Music/Cinematic Ambience/I'm Going Home.mp3
+/home/cano/Music/Cinematic Ambience/Sparse Party.mp3
+/home/cano/Music/Cinematic Ambience/Bed of Secrets.mp3
+/home/cano/Music/Cinematic Ambience/Not All Men (From ＂Women Talking＂ Soundtrack).mp3
+/home/cano/Music/Cinematic Ambience/Guðnadóttir： Mortar.mp3
+/home/cano/Music/Enfoque Electronico/SCB - Bone (Official Audio)-(p).mp3
+/home/cano/Music/Enfoque Electronico/Where Your Body Begins-(p).mp3
+/home/cano/Music/Concentracion Jazzera/H ZETTRIO ⧸ おだやかに (Gently) [Music Video]-(p).mp3
+/home/cano/Music/Enfoque Electronico/Bruce Brubaker, Max Cooper - Opening (Glassforms Version Edit)-(p).mp3
+/home/cano/Music/Cinematic Ambience/The Gravel Road (Score).mp3
+/home/cano/Music/Concentracion Jazzera/New Beginnings-(p).mp3
+/home/cano/Music/Concentracion Jazzera/＂Adrian, Ali and Gregory＂ by João Donato, Adrian Younge, and Ali Shaheed Muhammad-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Ugly Beauty-(p).mp3
+/home/cano/Music/Cinematic Ambience/Justin’s Theme.mp3
+/home/cano/Music/Cinematic Ambience/Passing By.mp3
+/home/cano/Music/Concentracion Jazzera/Samba de Maracatu-(p).mp3
+/home/cano/Music/Cinematic Ambience/Anamnesis (Single Edit).mp3
+/home/cano/Music/Concentracion Jazzera/Joey Alexander - SALT (Audio)-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Midnight Waves-(p).mp3
+/home/cano/Music/Cinematic Ambience/Hoa.mp3
+/home/cano/Music/Cinematic Ambience/Burden of the Crown.mp3
+/home/cano/Music/Cinematic Ambience/A New Era.mp3
+/home/cano/Music/Cinematic Ambience/That I Do Remember.mp3
+/home/cano/Music/Enfoque Electronico/Home-(p).mp3
+/home/cano/Music/Enfoque Electronico/Jon Hopkins - Emerald Rush (Official Video)-(p).mp3
+/home/cano/Music/Enfoque Electronico/Jon Hopkins - Dawn Chorus (Official Video)-(p).mp3
+/home/cano/Music/Cinematic Ambience/Psalm 22.mp3
+/home/cano/Music/Cinematic Ambience/West.mp3
+/home/cano/Music/Concentracion Jazzera/Invitation-(p).mp3
+/home/cano/Music/Cinematic Ambience/Hold.mp3
+/home/cano/Music/Concentracion Jazzera/Julian Lage - Saint Rose (Visualizer)-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Julius Rodriguez - Where Grace Abounds-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Answer Me (Live)-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Castles Made of Sand-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Age Of Ascent-(p).mp3
+/home/cano/Music/Enfoque Electronico/The Orb - Traumvogel 'Okie Dokie' Album-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Fine Wine-(p).mp3
+/home/cano/Music/Cinematic Ambience/When You Are Alone.mp3
+/home/cano/Music/Concentracion Jazzera/Lambert - Follow-(p).mp3
+/home/cano/Music/Cinematic Ambience/Do You Remember.mp3
+/home/cano/Music/Cinematic Ambience/Cuttlefish REM Phase.mp3
+/home/cano/Music/Enfoque Electronico/Forward a Zircon-(p).mp3
+/home/cano/Music/Enfoque Electronico/I Feel Space-(p).mp3
+/home/cano/Music/Enfoque Electronico/Logic1000 - Natural (feat. Big Ever) (Official Visualiser)-(p).mp3
+/home/cano/Music/Enfoque Electronico/Lone - Temples-(p).mp3
+/home/cano/Music/Cinematic Ambience/The Way of the Leaf.mp3
+/home/cano/Music/Cinematic Ambience/Follow Your Heart.mp3
+/home/cano/Music/Cinematic Ambience/The Banished Angel (From ＂The Musical Anthology of His Dark Materials＂ Series 3).mp3
+/home/cano/Music/Enfoque Electronico/Skara-(p).mp3
+/home/cano/Music/Concentracion Jazzera/London Plane-(p).mp3
+/home/cano/Music/Enfoque Electronico/Modern Driveway-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Connie Han - Prima Materia (Official Audio)-(p).mp3
+/home/cano/Music/Cinematic Ambience/Retribution.mp3
+/home/cano/Music/Concentracion Jazzera/Future Memories-(p).mp3
+/home/cano/Music/Enfoque Electronico/Maribou State - 'Rituals'-(p).mp3
+/home/cano/Music/Enfoque Electronico/Maribou State - 'Turnmills'-(p).mp3
+/home/cano/Music/Cinematic Ambience/Mr. V's Sonata.mp3
+/home/cano/Music/Cinematic Ambience/Actually Her.mp3
+/home/cano/Music/Cinematic Ambience/He Wasn't There (From ＂Black Panther： Wakanda Forever＂⧸Audio Only).mp3
+/home/cano/Music/Concentracion Jazzera/It's You I Like-(p).mp3
+/home/cano/Music/Cinematic Ambience/Below The Surface.mp3
+/home/cano/Music/Concentracion Jazzera/Matt Wilde - Pivot (Official Audio)-(p).mp3
+/home/cano/Music/Enfoque Electronico/Frankyeffe & Seismal D - Dopamine-(p).mp3
+/home/cano/Music/Enfoque Electronico/Jim Wallis - Europa - Max Cooper Remix (video by Julius Horsthuis)-(p).mp3
+/home/cano/Music/Enfoque Electronico/Surge-(p).mp3
+/home/cano/Music/Cinematic Ambience/Romance III.mp3
+/home/cano/Music/Concentracion Jazzera/Max Richter - And Some Will Fall (Official Visualiser)-(p).mp3
+/home/cano/Music/Enfoque Electronico/Opening Mind-(p).mp3
+/home/cano/Music/Cinematic Ambience/Haywood Ranch.mp3
+/home/cano/Music/Cinematic Ambience/Peter Parker Picked a Perilously Precarious Profession (from ＂Spider-Man： No Way Home＂ Soundtrack).mp3
+/home/cano/Music/Cinematic Ambience/Just Desert.mp3
+/home/cano/Music/Enfoque Electronico/Let in the Light-(p).mp3
+/home/cano/Music/Cinematic Ambience/The Story.mp3
+/home/cano/Music/Enfoque Electronico/Netzchase Nailway-(p).mp3
+/home/cano/Music/Enfoque Electronico/Nightscape-(p).mp3
+/home/cano/Music/Enfoque Electronico/Apparat - Black Water (Official Video)-(p).mp3
+/home/cano/Music/Cinematic Ambience/Red Wool Fibers (From The Motion Picture “Where The Crawdads Sing”).mp3
+/home/cano/Music/Concentracion Jazzera/Space 2-(p).mp3
+/home/cano/Music/Enfoque Electronico/Sunder-(p).mp3
+/home/cano/Music/Cinematic Ambience/Grindle's Ghost.mp3
+/home/cano/Music/Cinematic Ambience/Damian Marhulets - Magic Madness Sadness (Offizielles Musikvideo).mp3
+/home/cano/Music/Cinematic Ambience/I'm Cruella.mp3
+/home/cano/Music/Cinematic Ambience/Nick Cave & Warren Ellis - Pearly (Soundtrack From the Netflix Film Blonde).mp3
+/home/cano/Music/Cinematic Ambience/Hansu Sees Sunja.mp3
+/home/cano/Music/Enfoque Electronico/Amistad-(p).mp3
+/home/cano/Music/Cinematic Ambience/Boba Fett Theme (From ＂Star Wars： The Mandalorian＂).mp3
+/home/cano/Music/Concentracion Jazzera/Birds Eye-(p).mp3
+/home/cano/Music/Enfoque Electronico/Orbital - P.H.U.K.-(p).mp3
+/home/cano/Music/Enfoque Electronico/Orbital - Belfast (ANNA Ambient Remix) Visualiser-(p).mp3
+/home/cano/Music/Enfoque Electronico/The System-(p).mp3
+/home/cano/Music/Enfoque Electronico/Lady Notion-(p).mp3
+/home/cano/Music/Cinematic Ambience/What About This？.mp3
+/home/cano/Music/Enfoque Electronico/Paul Haslinger - Room 3 (Official Video)-(p).mp3
+/home/cano/Music/Enfoque Electronico/Since 77-(p).mp3
+/home/cano/Music/Cinematic Ambience/Peter Gregson - Ritual (Visualizer).mp3
+/home/cano/Music/Cinematic Ambience/Peter Gregson - Vision (Official Music Video).mp3
+/home/cano/Music/Enfoque Electronico/Non Hoi-(p).mp3
+/home/cano/Music/Enfoque Electronico/Coat-(p).mp3
+/home/cano/Music/Enfoque Electronico/Eyen-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Blue-(p).mp3
+/home/cano/Music/Enfoque Electronico/Night Mail (Ben Gomori Redux Radio Edit)-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Soulchill-(p).mp3
+/home/cano/Music/Cinematic Ambience/Making Moonlight.mp3
+/home/cano/Music/Enfoque Electronico/Sébastien Tellier - Stuck in a Summer Love (Young Marco Remix) (Official Audio)-(p).mp3
+/home/cano/Music/Enfoque Electronico/John Maus - The Combine (Official Video)-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Poolside (radio single)-(p).mp3
+/home/cano/Music/Enfoque Electronico/Dream Structures-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Sun of June-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Odysea-(p).mp3
+/home/cano/Music/Cinematic Ambience/The Adam Project.mp3
+/home/cano/Music/Enfoque Electronico/Rone & Flavien Berger - Polichinelle-(p).mp3
+/home/cano/Music/Enfoque Electronico/Aphex Twin - Heliosphan-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Give Me Back My Slippers-(p).mp3
+/home/cano/Music/Cinematic Ambience/Ryuichi Sakamoto – oxygen ｜ Exception (Soundtrack from the Netflix Anime Series).mp3
+/home/cano/Music/Cinematic Ambience/Seeing with New Eyes.mp3
+/home/cano/Music/Enfoque Electronico/Rival Consoles - Not really (LUZoSCURA LP)-(p).mp3
+/home/cano/Music/Cinematic Ambience/Shabaka - To The Moon ft. André 3000.mp3
+/home/cano/Music/Concentracion Jazzera/Montmorency-(p).mp3
+/home/cano/Music/Cinematic Ambience/Power and Purpose ｜ The Witcher： Season 2 (Soundtrack from the Netflix Original Series).mp3
+/home/cano/Music/Cinematic Ambience/Birnam Wood ｜ The Tragedy of Macbeth (Soundtrack from the Apple Original Film).mp3
+/home/cano/Music/Cinematic Ambience/In the World of My Breath ｜ After Yang (Original Motion Picture Soundtrack).mp3
+/home/cano/Music/Enfoque Electronico/Pantheon-(p).mp3
+/home/cano/Music/Cinematic Ambience/I Was On Your Side.mp3
+/home/cano/Music/Enfoque Electronico/Project Pablo - 'Napoletana' (Official Audio)-(p).mp3
+/home/cano/Music/Enfoque Electronico/Teen Daze - Translation - Edit (Official Visualizer)-(p).mp3
+/home/cano/Music/Enfoque Electronico/Business Car Park 9-(p).mp3
+/home/cano/Music/Enfoque Electronico/Terminal Ema-(p).mp3
+/home/cano/Music/Enfoque Electronico/Pot Noddle-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Walk Cycle-(p).mp3
+/home/cano/Music/Enfoque Electronico/The Knife - Silent Shout (Official Music Video)-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Moon Time-(p).mp3
+/home/cano/Music/Cinematic Ambience/24. Fireflies.mp3
+/home/cano/Music/Cinematic Ambience/Better Than The Original.mp3
+/home/cano/Music/Concentracion Jazzera/Feeling Unprepared-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Ambleside-(p).mp3
+/home/cano/Music/Cinematic Ambience/Burying the Medal.mp3
+/home/cano/Music/Concentracion Jazzera/New Year's Day-(p).mp3
+/home/cano/Music/Cinematic Ambience/You Don’t Have to Be Alone.mp3
+/home/cano/Music/Concentracion Jazzera/Triosence - Odd Times-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Your Nearness-(p).mp3
+/home/cano/Music/Enfoque Electronico/Reverence-(p).mp3
+/home/cano/Music/Enfoque Electronico/Rico's Helly (Retailored by Nourizadeh & Teasdale)-(p).mp3
+/home/cano/Music/Enfoque Electronico/Tiny Reminder No 1 (C-Pij Remix)-(p).mp3
+/home/cano/Music/Enfoque Electronico/Interspaced-(p).mp3
+/home/cano/Music/Enfoque Electronico/Underworld - Dirty Epic (Dirty Ambi)-(p).mp3
+/home/cano/Music/Enfoque Electronico/Beautiful Burnout-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Vince Guaraldi Trio - Manhã de Carnaval  (Take 1, Set 3)-(p).mp3
+/home/cano/Music/Concentracion Jazzera/What The Heart Wants-(p).mp3
+/home/cano/Music/Concentracion Jazzera/Bring it Back Home-(p).mp3
+/home/cano/Music/Enfoque Electronico/A Hollow Promise-(p).mp3
+/home/cano/Music/Cinematic Ambience/Godzilla vs Kong Official Soundtrack ｜ Antarctica - Tom Holkenborg ｜ WaterTower.mp3
+/home/cano/Music/Cinematic Ambience/DC League of Super-Pets Soundtrack ｜ Leaving Krypton - Steve Jablonsky ｜ WaterTower.mp3
+/home/cano/Music/Cinematic Ambience/Fantastic Beasts： The Secrets of Dumbledore Soundtrack ｜ The Promise - James Newton Howard.mp3
+/home/cano/Music/Cinematic Ambience/House of the Dragon Soundtrack ｜ The Prince That Was Promised  - Ramin Djawadi ｜ WaterTower.mp3
+/home/cano/Music/Cinematic Ambience/The Batman Official Soundtrack ｜ Catwoman - Michael Giacchino ｜ WaterTower.mp3
+/home/cano/Music/Enfoque Electronico/Bellini： Sonnambula-(p).mp3
+/home/cano/Music/Enfoque Electronico/Wordsworth-(p).mp3
+/home/cano/Music/Enfoque Electronico/Quem Sou Eu-(p).mp3
---
+++ rofi/applets/calc.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# File containing clipboard history
+history_file="$HOME/.cache/wl-clipboard-history"
+
+# Theme
+theme="$HOME/.config/rofi/configs/dmenu.rasi"
+
+title="Clipboard History"
+
+# Rofi command
+rofi_cmd() {
+    rofi -theme-str 'textbox-prompt-colon {str: "";}' \
+        -theme-str 'window {width: 720px;}' \
+        -theme "${theme}" \
+        -p "$title" \
+        -l 8 \
+        -show calc -no-show-match -no-sort
+}
+
+# Load clipboard history and pass to rofi
+run_rofi() {
+    rofi_cmd
+}
+
+# Copy the selected entry back to the clipboard
+copy_to_clipboard() {
+    selected="$1"
+    if [[ -n "$selected" ]]; then
+        echo -n "$selected" | wl-copy
+        notify-send -u low "Clipboard" "Copied to clipboard: $selected"
+    fi
+}
+
+# Main
+chosen="$(run_rofi)"
+if [[ -n "$chosen" ]]; then
+    copy_to_clipboard "$chosen"
+fi
